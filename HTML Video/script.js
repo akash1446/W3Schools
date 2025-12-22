8a2aaeafb64750cc7b4b5b5234c3bf83ab694ab9
akash1446 <guduriakash9@gmail.com>
2025-12-22 17:59:17 +0530

    add of the styles more adding of the scripting data

--- HTML Video/script.js
+++ HTML Video/script.js
@@ -1,6 +1,20 @@
-
-
 document.getElementById("demo").innerHTML="<h2>Hello World</h2>";
 window.alert(5+6);
 alert(20*30);
 console.log(50/4);
+
+let text = "";
+let i =1;
+
+while(i < 10){
+    text +=i;
+    i++;
+}
+
+document.getElementById("demo").innerHTML =text;
+
+let x = false
+
+let y = new Boolean(false);
+
+document.getElementById("demo").innerHTML = typeof x + "<br>" + typeof y;
---
+++ Html value Attribute/script.js
@@ -0,0 +1,19 @@
+let name = null;
+let text = "missing"
+let result = name ?? text;
+
+document.getElementById("oper").innerHTML="the name is" + result;
+
+
+let i = 5;
+
+for(let i = 0; i < 10; i++){
+
+}
+
+while(i < 10){
+    text +="The number is " +i;
+    i++;
+}
+document.getElementById("loops").innerHTML=text;
+
